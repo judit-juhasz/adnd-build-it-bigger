a9e27e7de0844333648f57b49465312335017677
Judit Juhasz <judit-juhasz@users.noreply.github.com>
2018-01-28 10:32:07 +0100

    Add free and paid versions

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,15 +1,10 @@
 package com.udacity.gradle.builditbigger;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-
-import name.juhasz.judit.udacity.jokefactory.Joker;
-import name.juhasz.judit.udacity.joketeller.JokeDisplayActivity;
-
 
 public class MainActivity extends AppCompatActivity {
 

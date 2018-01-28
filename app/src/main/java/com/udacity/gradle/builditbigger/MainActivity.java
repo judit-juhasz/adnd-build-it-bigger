dd1e4cf62622c33dda137365ef839c215145242b
Judit Juhasz <judit-juhasz@users.noreply.github.com>
2018-01-28 05:29:38 +0100

    Create a Java library

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import name.juhasz.judit.udacity.jokefactory.Joker;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,7 +42,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+
+        Joker joker = new Joker();
+
+        Toast.makeText(this, joker.tellJoke(), Toast.LENGTH_SHORT).show();
     }
 
 
---
+++ jokefactory/src/main/java/name/juhasz/judit/udacity/jokefactory/Joker.java
@@ -0,0 +1,10 @@
+package name.juhasz.judit.udacity.jokefactory;
+
+public class Joker {
+
+    public String tellJoke() {
+        return "Sleep with an open window tonight! 1400 mosquitos like that. " +
+                "420 mosquitos commented on it. 210 mosquitos shared this. " +
+                "One mosquito invited for the event. 2800 mosquitos will be attending the event.";
+    }
+}

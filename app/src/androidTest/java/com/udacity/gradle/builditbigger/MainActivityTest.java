d53b69454b8ae946e88e1e5703377b0c9920a8ff
Judit Juhasz <judit-juhasz@users.noreply.github.com>
2018-01-28 19:03:03 +0100

    Check if the downloaded joke was given to the joke teller activity

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/MainActivityTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/MainActivityTest.java
@@ -13,12 +13,16 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.intent.Intents.intended;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
-import static android.support.test.espresso.intent.matcher.IntentMatchers.hasExtraWithKey;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasExtra;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.hamcrest.Matchers.allOf;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
+    private static String staticTestJoke = "Sleep with an open window tonight! 1400 mosquitos like" +
+            " that. 420 mosquitos commented on it. 210 mosquitos shared this. One mosquito invited" +
+            " for the event. 2800 mosquitos will be attending the event.";
+
     @Rule
     public IntentsTestRule<MainActivity> mMainActivityTestRule =
             new IntentsTestRule<>(MainActivity.class);
@@ -31,10 +35,10 @@ public class MainActivityTest {
         // an intent extra.
         onView(withId(R.id.b_tell_joke)).perform(click());
 
-        // Then: The proper intent is started with the joke extra
+        // Then: The proper intent is started with the joke extra that was downloaded from the net
         intended(
                 allOf(hasComponent(JokeDisplayActivity.class.getCanonicalName()),
-                        hasExtraWithKey(JokeDisplayActivity.EXTRA_DATA)
+                        hasExtra(JokeDisplayActivity.EXTRA_DATA, staticTestJoke)
                 )
         );
     }
